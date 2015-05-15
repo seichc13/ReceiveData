bb166190ae9d5882e9ddbd45686f387c6966e458
Christopher Seil <seichc13@htl-kaindorf.ac.at>
2015-05-15 20:37:59 +0200

    First init

---
+++ receivedata.cpp
@@ -0,0 +1,5 @@
+#include "receivedata.h"
+
+ReceiveData::ReceiveData()
+{
+}
---
+++ receivedata.h
@@ -0,0 +1,10 @@
+#ifndef RECEIVEDATA_H
+#define RECEIVEDATA_H
+
+class ReceiveData
+{
+public:
+    ReceiveData();
+};
+
+#endif // RECEIVEDATA_H
